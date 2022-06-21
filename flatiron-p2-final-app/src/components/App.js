aa74c80316dbb6bc62443289ef1ffbe24e26228b
Badi-Haki Martin <jojackblack@gmail.com>
2022-06-21 03:23:34 -0400

    imported react-router-dom and wrapped the app in BrowserRouter

--- flatiron-p2-final-app/src/components/App.js
+++ flatiron-p2-final-app/src/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import logo from '../logo.svg';
 import '../App.css';
 import Home from './Home';
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
