8f16fea1db4a23448e65441fe8556b095f61328e
Badi-Haki Martin <jojackblack@gmail.com>
2022-06-21 14:41:09 -0400

    put switch component into App, starting development of route components

--- flatiron-p2-final-app/src/components/App.js
+++ flatiron-p2-final-app/src/components/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import '../App.css';
-import Home from './Home';
 import { Route, Switch } from "react-router-dom";
+import NavBar from './NavigationBar';
+import Home from './Home';
 
 function App() {
   return (
     <div className="App">
       {/* nav bar and nav links to all resources */}
+      <NavBar />
+      <Switch>
+        <Route>
+          {/* components here */}
+        </Route>
+      </Switch>
     </div>
   );
 }
