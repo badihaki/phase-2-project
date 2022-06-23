115f4a47ed44150739ddc966f8a50df6cb5b72bc
Badi-Haki Martin <jojackblack@gmail.com>
2022-06-23 10:15:06 -0400

    cleaned up tournamentlist component. Started styling navigation bar

--- flatiron-p2-final-app/src/components/TourneyTicket.js
+++ flatiron-p2-final-app/src/components/TourneyTicket.js
@@ -4,7 +4,9 @@ function TourneyTicket({ tournament }){
     return (
         <div>
             <h1>{tournament.name}</h1>
+            <h4>Organizer</h4>
             <h2>{tournament.organizer}</h2>
+            <h4>Organizer Email</h4>
             <h3>{tournament.email}</h3>
             {/*<h3>{tournament.date.toDateString()}</h3>*/}
         </div>
